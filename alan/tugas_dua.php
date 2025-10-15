bbf7e745b141bad2a01759fad7294ef68992e9aa
Alan raja zulkifli <alanrajazulkifli@gmail.com>
2025-10-15 13:38:24 +0700

    alan

--- alan/tugas_dua.php
+++ alan/tugas_dua.php
@@ -56,7 +56,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profil Alan Raja Zulkifli - 11 PPLG</title>
+    <title></title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
