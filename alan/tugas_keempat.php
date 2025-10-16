073b1a0e8c9e1540b54a324b8d790fe18127051e
Alan raja zulkifli <alanrajazulkifli@gmail.com>
2025-10-16 11:45:05 +0700

    alan

--- alan/tugas_keempat.php
+++ alan/tugas_keempat.php
@@ -4,7 +4,7 @@
        $a = $_POST['nis'];
        $b = $_POST['nama'];
        $c = $_POST['kelas'];
-        $qry = $conn->query("nis,nama,kelas");// silahkan kalian buat query di titik-titik..
+        $qry = $conn->query("INSERT INTO tb_siswa");// silahkan kalian buat query di titik-titik..
    if($qry == true){
         echo"<script>alert('Data Berhasil diinput....')</script>";
    }else{
