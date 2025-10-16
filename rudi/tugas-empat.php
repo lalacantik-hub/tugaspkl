2501108c88b0ad41629e43e0263a02fa909a2c13
cothefaza-technology <cs.getlink@gmail.com>
2025-10-16 12:02:10 +0700

    data baru

--- rudi/tugas-empat.php
+++ rudi/tugas-empat.php
@@ -12,12 +12,12 @@ if($qry == true){
     }      
 }
 ?>
-<form mothod="post">
+<form method="post">
     <label>NIS</label>
     <input type="text" name="nis">
     <label>Nama</label>
     <input type="text" name="nama">
     <label>Kelas</label>
     <input type="text" name="kelas">
-    <button type="submit">Submit</button>
+    <button type="submit" name="btn">Submit</button>
 </form>
