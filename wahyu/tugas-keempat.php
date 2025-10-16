ddea0ffdf57aebfe119851c7f9410f268918ef91
WahyoeVirlie <wahyuvirli252@gmail.com>
2025-10-16 12:02:11 +0700

    wahyu koneksi

--- wahyu/tugas-keempat.php
+++ wahyu/tugas-keempat.php
@@ -1,17 +1,4 @@
-<?php
-     include"koneksi.php";
-if(isset($_POST['btn'])){
-    $a = $_POST['nis'];
-    $b = $_POST['nama'];
-    $c = $_POST['kelas'];
-     $qry = $conn->query("INSERT INTO tb_siswa (nis, nama, kelas) VALUES ('$a','$b','$c',)");
-if($qry == true){
-     echo"<script>alert('Data Berhasil diinput....')</script>";
-}else{
-     echo"<script>alert('Data gagal diinput....')</script>";
-    }      
-}
-?>
+
 <form method="POST">
     <label>NIS</label>
     <input type="text" name="nis">
@@ -19,5 +6,22 @@ if($qry == true){
     <input type="text" name="nama">
     <label>Kelas</label>
     <input type="text" name="kelas">
-    <button type="submit">Submit</button>
+    <button type="submit" name="btn">Submit</button>
 </form>
+
+<?php
+     include"koneksi.php";
+
+
+if (isset($_POST['btn'])){
+    $a = $_POST['nis'];
+    $b = $_POST['nama'];
+    $c = $_POST['kelas'];
+     $qry = $conn->query("INSERT INTO tb_siswa (nis, nama, kelas) VALUES ('$a','$b','$c')");
+if($qry == true){
+     echo"<script>alert('Data Berhasil diinput....')</script>";
+}else{
+     echo"<script>alert('Data gagal diinput....')</script>";
+    }      
+}
+?>
