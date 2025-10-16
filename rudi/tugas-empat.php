d4045e7146d8dde75ffb428950c339eb2c195564
cothefaza-technology <cs.getlink@gmail.com>
2025-10-16 11:44:21 +0700

    query Insert

--- rudi/tugas-empat.php
+++ rudi/tugas-empat.php
@@ -1,3 +1,17 @@
+<?php
+     include"koneksi.php";
+if(isset($_POST['btn'])){
+    $a = $_POST['nis'];
+    $b = $_POST['nama'];
+    $c = $_POST['kelas']
+     $qry = $conn->query("INSERT INTO tb_siswa(nis,nama,kelas) VALUES('$a','$b','$c')");// silahkan kalian buat query di titik-titik..
+if($qry == true){
+     echo"<script>alert('Data Berhasil diinput....')</script>";
+}else{
+     echo"<script>alert('Data gagal diinput....')</script>";
+    }      
+}
+?>
 <form mothod="post">
     <label>NIS</label>
     <input type="text" name="nis">
