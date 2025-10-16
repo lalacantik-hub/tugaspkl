c21a3468fe3750e33b9fb6f44477db7aa4a4af2e
Alan raja zulkifli <alanrajazulkifli@gmail.com>
2025-10-16 07:34:50 +0700

    alan

--- alan/tugas_keempat.php
+++ alan/tugas_keempat.php
@@ -1,9 +1,9 @@
  <form mothod="post">
     <label>NIS</label>
-    <input type="text" name="nis">
+    <input type="text" name="nis"><br>
     <label>Nama</label>
-    <input type="text" name="nama">
+    <input type="text" name="nama"><br>
     <label>Kelas</label>
     <input type="text" name="kelas">
     <button type="submit">Submit</button>
-</form>`
+</form>
